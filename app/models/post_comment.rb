433243cdafb5132a299db7f561743628d902606a
EC2 Default User <ec2-user@ip-172-31-23-125.ap-northeast-1.compute.internal>
2024-07-06 16:22:49 +0000

    admin

---
+++ app/models/post_comment.rb
@@ -0,0 +1,8 @@
+class PostComment < ApplicationRecord
+  
+  belongs_to :user
+  belongs_to :post_image
+  
+  validates :user_id, uniqueness: {scope: :post_image_id}
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:show, :edit, :update]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+  sessions: 'admin/sessions'
+  }
+ 
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
 
-  root 'homes#top'
-
-  get "/homes/about" => "homes#about", as: "about"
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+  end
 
 end
